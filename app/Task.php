7400442141cf065cf218baebd02bda22d9e53209
TheRealJD3 <jasonshawndsouza@gmail.com>
2018-06-04 18:17:19 +0400

    Start from 017

---
+++ app/Task.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Task extends Model
+{
+    //
+    protected $fillable = [
+      'name',
+      'project_id',
+      'user_id',
+      'days',
+      'hours',
+      'company_id',
+      
+
+    ];
+}
